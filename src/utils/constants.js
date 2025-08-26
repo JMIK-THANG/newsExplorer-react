2c547f7ba0a4f1adf743ee7e21acb64fad0d753a
Jmik Thang <78833740+JMIK-THANG@users.noreply.github.com>
2025-08-25 21:59:39 -0400

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,5 @@
 export const NEWS_API_KEY = "eb7587ec1f5340989057975c6abf027b";
 export const NEWS_API_URL = "https://newsapi.org/v2/everything";
 
-export const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://api.news.jmik.crabdance.com"
-    : "http://localhost:3005";
+export const baseUrl =import.meta.env.VITE_API_BASE || "https://newsexplorer-backend-2.onrender.com";
+
