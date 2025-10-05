fa483e61ad438449c91ce49390f5026a8ef12ee1
Jmik Thang <jmikthang5@gmail.com>
2025-10-04 22:17:01 -0400

    update base URL for production environment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ const NEWS_API_URL = "https://newsapi.org/v2/everything";
 
 // const baseUrl =import.meta.env.VITE_API_BASE || "https://newsexplorer-backend-2.onrender.com";
 
-const baseUrl = import.meta.env.PROD ?  "https://newsexplorer-backend-2.onrender.com" : "http://localhost:3005"
+const baseUrl = import.meta.env.PROD ?  "https://newsexplorer-backend-8enw.onrender.com" : "http://localhost:3005"
 
 export{NEWS_API_KEY,NEWS_API_URL,baseUrl}
 
